b4e414a444fa39dd91ac4b91181b01f16cf3a642
kinsly_cho <kinslycho@gmail.com>
2025-07-16 16:41:29 +0100

    setting up services, routes and components

--- src/app/features/vote/vote.model.ts
+++ src/app/features/vote/vote.model.ts
@@ -1,3 +1,5 @@
+import {User} from "../user/user.model";
+
 export interface Vote {
     id?: string;
     title: string;
@@ -15,8 +17,20 @@ export interface VoteCandidate {
 
 export interface VoteResponse<T> {
     success: boolean;
-    data: T;
-    message?: string[];
+    data?: T;
+    message?: string[] | string;
     statusCode: number;
 }
 
+export interface VoteResult{
+    id?: string;
+    name: string;
+    count: number;
+}
+
+export interface Voter{
+    id?: string;
+    user: User;
+    candidate: VoteCandidate,
+    createdAt: Date;
+}
--- src/app/features/vote/vote.service.ts
+++ src/app/features/vote/vote.service.ts
@@ -2,24 +2,40 @@ import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs";
-import {Vote, VoteResponse} from "./vote.model";
+import {Vote, Voter, VoteResponse, VoteResult} from "./vote.model";
 
 @Injectable({
     providedIn: 'root',
 })
 export class AnonceService {
     private baseUrl = environment.apiUrl;
-    private apiUrl = this.baseUrl + 'anonces';
+    private apiUrl = this.baseUrl + 'votes';
 
     constructor(private http: HttpClient) {
     }
 
-    getAll(): Observable<VoteResponse<Vote[]>> {
-        return this.http.get<VoteResponse<Vote[]>>(`${this.apiUrl}`,)
+    getAll(isActive: boolean): Observable<VoteResponse<Vote[]>> {
+        return this.http.get<VoteResponse<Vote[]>>(`${this.apiUrl}?isActive=${isActive}`,)
     }
 
     add(vote: Vote): Observable<VoteResponse<Vote>> {
         return this.http.post<VoteResponse<Vote>>(`${this.apiUrl}`, vote)
+    }
+
+    cast(voteId: string, candidateId:string): Observable<VoteResponse<Vote>> {
+        return this.http.post<VoteResponse<Vote>>(`${this.apiUrl}/cast`, {voteId, candidateId})
+    }
+
+    results(voteId: string): Observable<VoteResponse<VoteResult[]>> {
+        return this.http.get<VoteResponse<VoteResult[]>>(`${this.apiUrl}/${voteId}/results`,)
+    }
+
+    voters(voteId: string): Observable<VoteResponse<Voter[]>> {
+        return this.http.get<VoteResponse<Voter[]>>(`${this.apiUrl}/${voteId}/voters`,)
+    }
+
+    activate(voteId: string, isActive: boolean): Observable<VoteResponse<Vote>> {
+        return this.http.patch<VoteResponse<Vote>>(`${this.apiUrl}`,{voteId, isActive})
     }
 
 
